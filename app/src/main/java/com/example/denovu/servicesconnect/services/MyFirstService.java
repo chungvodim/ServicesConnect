030533a58d4c962feb19ab048e5b5b59e1ce194a
chungvodim <ba198647@gmail.com>
2016-10-07 16:47:26 +0700

    Init

---
+++ app/src/main/java/com/example/denovu/servicesconnect/services/MyFirstService.java
@@ -0,0 +1,30 @@
+package com.example.denovu.servicesconnect.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by Wellcome on 10/7/2016.
+ */
+
+public class MyFirstService extends Service {
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
